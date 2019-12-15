d9c1a7d720f7f8f8a57204f48997b5f4afb314f0
colin <colin@thinkr.fr>
2019-12-15 21:22:12 +0100

    added manifest

--- inst/app/www/script.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-  function watchColorPicker(event) {
-    document.querySelectorAll("p").forEach(function(p) {
-      p.style.color = event.target.value;
-    });
-  }
-});
