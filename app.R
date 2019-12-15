bf1cc6cf0e1b1b2bcfee30e3b3fc95c806c16b26
colin <colin@thinkr.fr>
2019-12-15 22:44:02 +0100

    namespace

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-# Launch the ShinyApp (Do not remove this comment)
+  # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
