086ca768c8070972e3943d68f27df23c5aaef032
colin <colin@thinkr.fr>
2019-12-20 16:54:33 +0100

    new manifest

--- app.R
+++ app.R
@@ -2,10 +2,6 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 # 
-if (packageVersion("magick") != "2.2"){
-  remotes::install_github("ropensci/magick")
-}
-
 pkgload::load_all()
 options( "golem.app.prod" = TRUE)
 hexmake::run_app() # add parameters here (if any)
