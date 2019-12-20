ac4ad9d553550e98fb242ab3d94cce5ac1a1b355
colin <colin@thinkr.fr>
2019-12-20 16:51:38 +0100

    tried that weird thing

--- app.R
+++ app.R
@@ -1,6 +1,10 @@
   # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
+# 
+if (packageVersion("magick") != "2.2"){
+  remotes::install_github("ropensci/magick")
+}
 
 pkgload::load_all()
 options( "golem.app.prod" = TRUE)
